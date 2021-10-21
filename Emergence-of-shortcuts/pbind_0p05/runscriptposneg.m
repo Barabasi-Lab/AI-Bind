93a76f5f8fe3d51395839f694e4c767c8047b311
Ayan Chatterjee <chatterjee.ay@northeastern.edu>
2021-10-21 14:55:20 -0400

    Adding analysis on the emergence of topological shortcuts + SI figures

---
+++ Emergence-of-shortcuts/pbind_0p05/runscriptposneg.m
@@ -0,0 +1,22 @@
+%% load('datadegree')
+load('degreetrain01targets.txt');
+load('degreetrain10targets.txt');
+
+%%
+[t01, t10, k01cal,k10cal,summat01,summat10]=multidegree_entropy_pos_neg(degreetrain01targets, degreetrain10targets);
+
+%%
+figure,
+
+hold on
+plot(degreetrain01targets, k01cal, 'o')
+hold on
+plot(degreetrain10targets, k10cal, 'o')
+
+xlabel('real degree')
+ylabel('predicted degree')
+
+
+%%
+writematrix(summat10,'C:\Users\Ayan\Documents\GitHub\AI-Bind\Emergence-of-shortcuts\Unipartite\pbind_0p05\summat10.csv')
+writematrix(summat01,'C:\Users\Ayan\Documents\GitHub\AI-Bind\Emergence-of-shortcuts\Unipartite\pbind_0p05\summat01.csv')
